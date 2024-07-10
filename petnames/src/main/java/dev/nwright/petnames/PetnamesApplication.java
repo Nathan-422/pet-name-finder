70c39a248e1cd66d3d411e039cd9af631b50e7ca
Nathan Wright <natiouswright@gmail.com>
2024-07-09 20:01:20 -0500

    Initial commit

---
+++ petnames/src/main/java/dev/nwright/petnames/PetnamesApplication.java
@@ -0,0 +1,13 @@
+package dev.nwright.petnames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetnamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetnamesApplication.class, args);
+	}
+
+}
---
+++ petnames/src/test/java/dev/nwright/petnames/PetnamesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nwright.petnames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetnamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
