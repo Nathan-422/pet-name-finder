0070364395d12da38509bdd2910b7f7dd05ed91d
Nathan Wright <natiouswright@gmail.com>
2024-07-28 15:43:34 -0500

    Redo security chain for 6.0
    Changed out methods deprecated in 6.0

--- petnames/src/main/java/dev/nwright/petnames/config/SecurityConfiguration.java
+++ petnames/src/main/java/dev/nwright/petnames/config/SecurityConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,11 +19,6 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @EnableWebSecurity
 
@@ -39,15 +35,16 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf().disable()
-        .authorizeRequests()
-        .antMatchers("/**/auth/**")
-        .permitAll()
-        .anyRequest()
-        .authenticated().and().cors(Customizer.withDefaults())
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        .and()
+        .csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+            .requestMatchers("/**/auth/**")
+            .permitAll())
+        .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+            .anyRequest()
+            .authenticated())
+        .cors(Customizer.withDefaults())
+        .sessionManagement((sessionManagement) -> sessionManagement
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(authenticationProvider())
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     // .cors(Customizer.withDefaults())
